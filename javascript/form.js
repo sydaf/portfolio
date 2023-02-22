64f06ece8b62efa3ebca48ab80e03c623a3f78b6
Syed Ahmad Fakrullah <syedahmadfakhrullah@gmail.com>
2023-02-22 21:16:28 +0800

    Pop up alert for form submit

--- javascript/form.js
+++ javascript/form.js
@@ -3,12 +3,13 @@ const email = document.getElementById('email_id').value;
 const message = document.getElementById('message').value;
 const btn = document.getElementById('submitBtn');
 const form = document.getElementById("contactMe");
+const popup = document.getElementById('popUp');
 
 function submitForm(event) {
     event.preventDefault();
 };
 
-form.addEventListener('submit', submitForm)
+form.addEventListener('submit', submitForm);
 
 function SendMail() {
     var params = {
@@ -16,14 +17,17 @@ function SendMail() {
         email_id: document.getElementById("email_id").value,
         message: document.getElementById("message").value,
     };
+    
     emailjs.send("service_f8rj0nc", "template_7w1dbmr", params)
     .then(function (res) {
-        alert("Success! " + res.status);
+        popup.classList.toggle('trigger')
     }, function(error) {
         alert("Failed! " + res.status)
     });
+
+    form.reset();
 };
 
-form.reset();
-
-console.log(nameForm.value);
+function closeCard() {
+    popup.classList.toggle('trigger')
+}
